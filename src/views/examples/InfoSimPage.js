7fb9f061605b90ccc32919974fe16524a2efa3fa
Samantha Bravo <sbravo1@ucol.mx>
2023-05-02 09:28:53 -0600

    texto

--- src/views/examples/InfoSimPage.js
+++ src/views/examples/InfoSimPage.js
@@ -4,6 +4,8 @@ import React from "react";
 
 import {
     Button,
+    Col,
+    Row
     // Input,
     // InputGroupAddon,
     // InputGroupText,
@@ -33,7 +35,8 @@ function InfoSimPage(){
         <div >
             <div className="" style={{marginBottom: '80px'}}>
                 <h1 className="display-4 " style={{marginTop: '160px', textAlign: 'center'}}>Simulador StatCast</h1>
-                <div >
+                <Row>
+                <Col >
                     <button className="btn-inf">1er Promedio Movil</button><br></br>
                     <button className="btn-inf" >2do Promedio Movil</button><br></br>
                     <button className="btn-inf" >Promedio Movil Doble</button><br></br>
@@ -41,9 +44,16 @@ function InfoSimPage(){
                     <button className="btn-inf" >Simple Brown</button><br></br>
                     <button className="btn-inf" >Doble Holt</button><br></br>
                     <button className="btn-inf" >Triple Winter</button><br></br>
-
-                </div>
-
+                </Col>
+                <Col>
+                    <h1 className="text-btn">"Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+                     nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+                     mollit anim id est laborum."</h1>
+                    </Col>
+                </Row>
                 <div>
                 <Button  className="btn-simu" href="/simulador-pm" > Ir al simulador </Button>
                 </div>
