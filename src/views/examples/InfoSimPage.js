9c3bb213ca3633ddbd2262f4fc838d73b3323dd6
Josselin Valdez <jvaldez13@ucol.mx>
2023-05-02 09:40:45 -0600

    texto de info

--- src/views/examples/InfoSimPage.js
+++ src/views/examples/InfoSimPage.js
@@ -35,25 +35,30 @@ function InfoSimPage(){
         <div >
             <div className="" style={{marginBottom: '80px'}}>
                 <h1 className="display-4 " style={{marginTop: '160px', textAlign: 'center'}}>Simulador StatCast</h1>
-                <Row>
-                <Col >
-                    <button className="btn-inf">1er Promedio Movil</button><br></br>
-                    <button className="btn-inf" >2do Promedio Movil</button><br></br>
-                    <button className="btn-inf" >Promedio Movil Doble</button><br></br>
-                    <button className="btn-inf" >Promedio Movil Ponderado</button><br></br>
-                    <button className="btn-inf" >Simple Brown</button><br></br>
-                    <button className="btn-inf" >Doble Holt</button><br></br>
-                    <button className="btn-inf" >Triple Winter</button><br></br>
-                </Col>
-                <Col>
-                    <h1 className="text-btn">"Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-                     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-                     nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-                     mollit anim id est laborum."</h1>
-                    </Col>
-                </Row>
+                <div className="contenedor">
+                    <div >
+                        <button className="btn-inf">1er Promedio Movil</button><br></br>
+                        <button className="btn-inf" >2do Promedio Movil</button><br></br>
+                        <button className="btn-inf" >Promedio Movil Doble</button><br></br>
+                        <button className="btn-inf" >Promedio Movil Ponderado</button><br></br>
+                        <button className="btn-inf" >Simple Brown</button><br></br>
+                        <button className="btn-inf" >Doble Holt</button><br></br>
+                        <button className="btn-inf" >Triple Winter</button><br></br>
+                    </div>
+                    <div>
+                        <h1 className="text-btn">"Lorem ipsum dolor sit amet, 
+                        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+                        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+                        mollit anim id est laborum.Lorem ipsum dolor sit amet, 
+                        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+                        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+                        mollit anim id est laborum."</h1>
+                    </div>
+                </div>
                 <div>
                 <Button  className="btn-simu" href="/simulador-pm" > Ir al simulador </Button>
                 </div>
